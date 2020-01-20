689b3b469972825457cef7486f0afd13730506b2
Almeida <blackheal@gmail.com>
2020-01-20 02:02:58 -0300

    Counters Pt01

---
+++ Lecture14/app.js
@@ -0,0 +1,48 @@
+(function() {
+    'use strict';
+    angular.module('CounterApp', [])
+    .controller('CounterController', CounterController);
+
+    CounterController.$inject = ['$scope'];
+    function CounterController($scope) {
+        let vm = this;
+        vm.onceCounter = 0;
+        vm.counter = 0
+
+        vm.showNumberOfWatchers = function() {
+            console.log("# of Watchers = " + $scope.$$watchersCount);
+        }
+
+        vm.countOnce = function() {
+            vm.onceCounter = 1;
+        }
+
+        vm.upCounter = function() {
+          vm.counter ++;  
+        };
+        
+        // Nocite that instead of vm, 
+        // you need to use the name you gave in the 'controllerAS declaration'
+        // or use the next method wich watches the controllerAs object and 
+        // return the variable you and to watch
+        $scope.$watch('Ctrl.onceCounter', function(newValue, oldValue) {
+            console.log("onceCounter old Value: ", oldValue);
+            console.log("onceCounter new Value: ", newValue);
+        });
+        
+        /*$scope.$watch(function watch(scope) {
+            return vm.onceCounter;
+        }, function handle(newValue, oldValue) {
+            console.log("old Value: ", oldValue);
+            console.log("new Value: ", newValue);
+        }, true);*/
+
+        $scope.$watch('Ctrl.counter', function(newValue, oldValue) {
+            console.log("counter old Value: ", oldValue);
+            console.log("counter new Value: ", newValue);
+        }, true);
+
+    }
+
+
+})();
